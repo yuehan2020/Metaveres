f8cbcadabf3fae3f2d25adbe475f27a54b56a874
小涵 <1160772681@qq.com>
2022-04-13 22:17:26 +0800

    导航栏阴影修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -146,7 +146,7 @@ export default {
       width: 100vw;
       top: 0;
       position: fixed;
-      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+      box-shadow: 0 2px 4px rgb(0 0 0 / 8%);
     }
   }
 }
